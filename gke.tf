8c36f6ddc81771e596140ded58c78ea066c32e89
sharanya.gandla <gandla.sharanya@ncr.com>
2021-07-28 00:49:42 +0530

    updating code for workload creation

--- gke.tf
+++ gke.tf
@@ -81,26 +81,26 @@ resource "google_sql_user" "sql-user" {
 }
 
 # using the preexisting service account
-# resource "google_service_account" "GA-ServiceAccount" {
-#   account_id   = "GA-ServiceAccount"
-# }
+resource "google_service_account" "GA-ServiceAccount" {
+  account_id   = "ga-ServiceAccount"
+}
 
 # to create a workload
-# module "hack-infinities-workload-identity" {
-#   source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
-#   use_existing_gcp_sa = true
-#   name                = google_service_account.GA-ServiceAccount.account_id
-#   project_id          = var.project_id
-# }
+module "my-app-workload-identity" {
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  use_existing_gcp_sa = true
+  name                = google_service_account.GA-ServiceAccount.account_id
+  project_id          = var.project_id
+}
 
 # workload creation with creating a new service account
-module "hack-infinities-workload-identity" {
-  source     = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
-  name       = "workload-ServiceAccount"
-  namespace  = "default"
-  project_id = "${var.project_id}"
-  roles      = ["roles/storage.Admin", "roles/compute.Admin"]
-}
+# module "hack-infinities-workload-identity" {
+#   source     = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+#   name       = "workload-ServiceAccount"
+#   namespace  = "default"
+#   project_id = "${var.project_id}"
+#   roles      = ["roles/storage.Admin", "roles/compute.Admin"]
+# }
 
 # # Kubernetes provider
 # # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
