21f29ca074ec0a424afff28291c2caf2e12a387e
sharanya.gandla <gandla.sharanya@ncr.com>
2021-07-28 13:53:02 +0530

    commenting node pool code

--- gke.tf
+++ gke.tf
@@ -29,30 +29,30 @@ resource "google_container_cluster" "primary" {
 }
 
 # Separately Managed Node Pool
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "${google_container_cluster.primary.name}-node-pool"
-  location   = var.region
-  cluster    = google_container_cluster.primary.name
-  node_count = var.gke_num_nodes
+# resource "google_container_node_pool" "primary_nodes" {
+#   name       = "${google_container_cluster.primary.name}-node-pool"
+#   location   = var.region
+#   cluster    = google_container_cluster.primary.name
+#   node_count = var.gke_num_nodes
 
-  node_config {
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+#   node_config {
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/logging.write",
+#       "https://www.googleapis.com/auth/monitoring",
+#     ]
 
-    labels = {
-      env = var.project_id
-    }
+#     labels = {
+#       env = var.project_id
+#     }
 
-    # preemptible  = true
-    machine_type = "n1-standard-1"
-    tags         = ["gke-node", "${var.project_id}-gke"]
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-  }
-}
+#     # preemptible  = true
+#     machine_type = "n1-standard-1"
+#     tags         = ["gke-node", "${var.project_id}-gke"]
+#     metadata = {
+#       disable-legacy-endpoints = "true"
+#     }
+#   }
+# }
 
 # creating sql instance
 resource "google_sql_database_instance" "mysql" {
@@ -80,6 +80,7 @@ resource "google_sql_user" "sql-user" {
   password = var.sql_password
 }
 
+# creating a workload identity pool
 resource "google_iam_workload_identity_pool" "workload_identity" {
   provider                  = google-beta
   workload_identity_pool_id = module.hack-hsp-infinities-workload-identity.name
@@ -111,23 +112,3 @@ provider "kubernetes" {
     data.google_container_cluster.default.master_auth[0].cluster_ca_certificate,
   )
 }
-
-# # Kubernetes provider
-# # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
-# # It references the variables and resources provisioned in this file. 
-# # We recommend you put this in another file -- so you can have a more modular configuration.
-# # https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster#optional-configure-terraform-kubernetes-provider
-# # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/tutorials/terraform/kubernetes-provider.
-
-# provider "kubernetes" {
-#   load_config_file = "false"
-
-#   host     = google_container_cluster.primary.endpoint
-#   username = var.gke_username
-#   password = var.gke_password
-
-#   client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
-#   client_key             = google_container_cluster.primary.master_auth.0.client_key
-#   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-# }
-
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,2 @@
 project_id = "hack-hsp-infinities"
-region     = "us-central1"
+region     = "us-central1a"
