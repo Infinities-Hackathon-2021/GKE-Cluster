6545d4c6e07a195a180231f181e945f0d7ebce09
pg185155 <pg185155@ncr.com>
2021-07-28 21:46:16 +0530

    Creating node pool

--- gke.tf
+++ gke.tf
@@ -29,30 +29,30 @@ resource "google_container_cluster" "primary" {
 }
 
 # Separately Managed Node Pool
-# resource "google_container_node_pool" "primary_nodes" {
-#   name       = "${google_container_cluster.primary.name}-node-pool"
-#   location   = var.region
-#   cluster    = google_container_cluster.primary.name
-#   node_count = var.gke_num_nodes
-
-#   node_config {
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/logging.write",
-#       "https://www.googleapis.com/auth/monitoring",
-#     ]
-
-#     labels = {
-#       env = var.project_id
-#     }
-
-#     # preemptible  = true
-#     machine_type = "n1-standard-1"
-#     tags         = ["gke-node", "${var.project_id}-gke"]
-#     metadata = {
-#       disable-legacy-endpoints = "true"
-#     }
-#   }
-# }
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${google_container_cluster.primary.name}-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = var.gke_num_nodes
+# 
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+# 
+    labels = {
+      env = var.project_id
+    }
+# 
+    # preemptible  = true
+    machine_type = "n1-standard-1"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
 
 # creating sql instance
 resource "google_sql_database_instance" "mysql" {
